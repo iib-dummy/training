70a10111d3486b50bfbe99cda4f22fd5ffead7d9
Victor Cristoph Brincas Sinkunas <brvi1017@hs-karlsruhe.de>
2019-11-14 16:30:19 +0100

    add ui file

---
+++ src/vicropht/ui.tsx
@@ -0,0 +1,8 @@
+import { Container, createStyles, makeStyles } from '@material-ui/core'
+import React from 'react'
+
+import Jokes from './Components/Jokes/Jokes'
+import JokesButton from './Components/Jokes/JokesButton'
+import JokesHeader from './Components/Jokes/JokesHeader'
+import useJokesReducer from './Components/Jokes/JokesReducer'
+import LoginButton from './Components/LoginButton'
