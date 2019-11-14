b6ba6ae25f2a2dcc00111a65a8535cfbe23f4186
wiju1013wj <33145568+wiju1013wj@users.noreply.github.com>
2019-11-14 17:04:44 +0100

    AnlegenAnfang
    Anlegen Ordner mit Gruppenname, samt Einstiegskomponente

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { Box, Divider, Typography } from '@material-ui/core'
 import React from 'react'
 
-import Fhi from './fhi/Fhi'
-
+import Wiju1013wj from './wiju1013wj/Wiju1013wj'
 const App = () => (
     <Box margin={4}>
         <Typography variant="h6" gutterBottom align="center">
-            heute ist der {new Date().toLocaleDateString()} - viel Spaß
+            Heute ist der {new Date().toLocaleDateString()} - Viel Spaß
         </Typography>
         <Divider />
         {/* ab hier bitte eure "Einstiegskomponente" einfügen, bspw.: */}
-        <Fhi />
+        <Wiju1013wj />
     </Box>
 )
 
