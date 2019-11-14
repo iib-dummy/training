3bd1ecc1e8ab7e5eea7af426637779b68a17c3f9
Victor Cristoph Brincas Sinkunas <brvi1017@hs-karlsruhe.de>
2019-11-14 17:27:45 +0100

    benutze text field

--- src/vicropht/vicropht.tsx
+++ src/vicropht/vicropht.tsx
@@ -3,4 +3,26 @@ import React from 'react'
 
 const vicropht = () => <TextField margin="dense" fullWidth label="vicropht" />
 
+class MyForm extends React.Component {
+    constructor() {
+        super()
+        this.state = { id: null }
+    }
+    handleChange = event => {
+        this.setState({ id: event.target.value })
+    }
+    handleSubmit = event => {
+        //Make a network call somewhere
+        event.preventDefault()
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <TextField floatingLabelText="ID Number" onChange={this.handleChange} />
+                <RaisedButton label="Submit" type="submit" />
+            </form>
+        )
+    }
+}
+
 export default vicropht
